e5b854e2ed0e55619357546cfd69ad636003681d
AntonyJR <A.Reynolds@computer.org>
2023-07-19 18:13:35 -0600

    Make test script results easier to read

--- test.sh
+++ test.sh
@@ -1,26 +1,41 @@
 #!/bin/bash
 
 echo help
+echo ==========================================================
 python3 main.py --help
+
+echo
 echo subscribed_regions json
+echo ==========================================================
 python3 main.py subscribed_regions | wc -l
+
+echo
 echo subscribed_regions array
+echo ==========================================================
 python3 main.py  subscribed_regions --param_format
+
+echo
 echo searchable_resource_types json
+echo ==========================================================
 python3 main.py searchable_resource_types | wc -l
+
+echo
 echo searchable_resource_types array
+echo ==========================================================
 python3 main.py searchable_resource_types --param_format
+
+echo
 echo search_resources json
+echo ==========================================================
 python3 main.py search_resources --resource_type IntegrationInstance --resource_type AutonomousDatabase | wc -l
-echo search_resources with jq
-python3 main.py search_resources --resource_type IntegrationInstance --resource_type AutonomousDatabase |
-jq '.[] | {"display_name":.display_name, "identifier":.identifier, "compartment_id":.compartment_id}'
-echo search_resources across al regions with jq
+
+echo
+echo search_resources json across all regions
+python3 main.py search_resources --resource_type IntegrationInstance --resource_type AutonomousDatabase --all_regions |
+ wc -l
+
+echo
+echo search_resources json across all regions with jq
+echo ==========================================================
 python3 main.py search_resources --resource_type IntegrationInstance --resource_type AutonomousDatabase --all_regions |
 jq '.[] | {"display_name":.display_name, "identifier":.identifier, "compartment_id":.compartment_id}'
-echo search_resources with jq
-python3 main.py search_resources --resource_type IntegrationInstance --resource_type AutonomousDatabase |
-jq '.[] | {"display_name":.display_name, "identifier":.identifier, "compartment_id":.compartment_id}' | wc -l
-echo search_resources across al regions with jq
-python3 main.py search_resources --resource_type IntegrationInstance --resource_type AutonomousDatabase --all_regions |
-jq '.[] | {"display_name":.display_name, "identifier":.identifier, "compartment_id":.compartment_id}' | wc -l
